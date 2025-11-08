a9e5ec0f545c41b3850bf65b7e70f04d1407a089
Timur5092 <11tima1188@gmail.com>
2025-11-09 01:47:23 +0300

    Add main logic with colorama demo

--- lecture_1/my_project/main.py
+++ lecture_1/my_project/main.py
@@ -1,1 +1,9 @@
+from colorama import init, Fore, Back, Style
+
+init()
+
+print(f"{Fore.RED}{Back.YELLOW}Hello World!{Style.RESET_ALL}")
+print(f"{Fore.GREEN}{Back.WHITE}Hello World!{Style.RESET_ALL}")
+print(f"{Fore.BLUE}{Back.WHITE}Hello World in Bright Blue!{Style.RESET_ALL}")
+print(f"{Fore.MAGENTA}{Back.CYAN}Hello World with Magenta text and Cyan background!{Style.RESET_ALL}")
 
